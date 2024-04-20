25523622d394c6573ccc5a1c2a13e9eda54ea7b6
Ruslan Basyrov <rusruo@gmail.com>
2024-04-20 17:06:30 -0400

    Uploaded the files and task description of project 3

---
+++ Project 3/project3.py
@@ -0,0 +1,39 @@
+"""
+Math 560
+Project 3
+Spring 2024
+
+Ruslan Basyrov
+Date: April 20 2024
+"""
+
+# Import math and p3tests.
+import math
+from p3tests import *
+
+################################################################################
+
+"""
+detectArbitrage
+"""
+def detectArbitrage(adjList, adjMat, tol=1e-15):
+    ##### Your implementation goes here. #####
+    return []
+    ##### Your implementation goes here. #####
+
+################################################################################
+
+"""
+rates2mat
+"""
+def rates2mat(rates):
+    ##### Your implementation goes here. #####
+    # Currently this only returns a copy of the rates matrix.
+    return [[R for R in row] for row in rates]
+    ##### Your implementation goes here. #####
+
+"""
+Main function.
+"""
+if __name__ == "__main__":
+    testRates()
